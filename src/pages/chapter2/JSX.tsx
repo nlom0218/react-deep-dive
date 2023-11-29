717dc619704155f231718482a64373fa6ae70904
nlom0218 <nlom0218@naver.com>
2023-11-29 23:52:52 +0900

    chore: 2장 리액트 핵심 요소 깊게 살펴보기 - JSX란?

--- src/pages/chapter2/JSX.tsx
+++ src/pages/chapter2/JSX.tsx
@@ -1,5 +1,53 @@
+import React, { PropsWithChildren } from 'react';
+
 const Jsx = () => {
-  return <div></div>;
+  return (
+    <div>
+      <$></$>
+      <_></_>
+      <JSXChild />
+      <TexOrHeading isHeading={true} children="Heading" />
+      <TexOrHeading isHeading={false} children="Not Heading" />
+      <TextOrHeading2 isHeading={true} />
+      <TextOrHeading2 isHeading={false} />
+    </div>
+  );
 };
 
 export default Jsx;
+
+function $() {
+  return <div>$</div>;
+}
+
+function _() {
+  return <div>_</div>;
+}
+
+const JSXChild = () => {
+  return <>{(() => 'foo')()}</>;
+};
+
+function TexOrHeading({
+  isHeading,
+  children,
+}: PropsWithChildren<{
+  isHeading: boolean;
+}>) {
+  return React.createElement(
+    isHeading ? 'h1' : 'span',
+    { className: 'text' },
+    children
+  );
+}
+
+function TextOrHeading2({
+  isHeading,
+  children,
+}: PropsWithChildren<{ isHeading: boolean }>) {
+  return isHeading ? (
+    <h1 className="text">{children}</h1>
+  ) : (
+    <span className="text">{children}</span>
+  );
+}
